2833f3febc4e46708c25120f1710318eb364bd30
ELLA LAKSHMAN <laxmannani960@gmail.com>
2024-12-01 16:03:14 +0530

    added course pages

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const userModel = require("./models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const cookie = require("cookie-parser");
+const cookieParser = require("cookie-parser");
+const { render } = require("ejs");
 
 const app = express();
 
@@ -13,42 +14,75 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser()); // Ensure cookie-parser is used correctly
 
 // Routes
 app.get("/", (req, res) => {
   res.render("login"); // Create a "login.ejs" template
 });
+
 app.get("/register", (req, res) => {
   res.render("register"); // Create a "register.ejs" template
 });
+
+// Route to delete a user by email
 app.get("/delete", async (req, res) => {
   const result = await userModel.deleteOne({ email: "unix@gmail.com" });
   if (result.deletedCount === 0) {
     return res.status(404).send("User not found");
   }
+  res.send("User deleted successfully");
 });
 
+app.get("/courses", isloggedin, (req, res) => {
+  res.render("courses");
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+    httpOnly: true,
+    path: "/",
+  });
+  res.redirect("/");
+});
+
+function isloggedin(req, res, next) {
+  const token = req.cookies.token;
+  if (!token) {
+    res.redirect("/");
+  }
+  next();
+}
+
+// Register a new user
 app.post("/create", async (req, res) => {
-  console.log(req.body);
   const { email, password, confirmpassword } = req.body;
 
+  // Validate password match
   if (password !== confirmpassword) {
     return res.status(400).send("Passwords do not match");
   }
+
   bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      return res.status(500).send("Error generating salt");
+    }
     bcrypt.hash(password, salt, async (err, hash) => {
+      if (err) {
+        return res.status(500).send("Error hashing password");
+      }
       try {
         const user = await userModel.create({
           email,
           password: hash,
-          confirmpassword: hash,
         });
+
         const token = jwt.sign({ email }, "shhhh");
-        res.cookie("token", token, { httpOnly: true, secure: true });
-        res.redirect("/");
+        res.cookie("token", token, { httpOnly: true, path: "/" }); // Ensure path matches
+        res.redirect("/courses");
       } catch (err) {
-        res.send("Error saving user:", err);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send("Error saving user: " + err.message);
       }
     });
   });
@@ -56,11 +90,13 @@ app.post("/create", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
+
   try {
     const user = await userModel.findOne({ email });
     if (!user) {
       return res.status(400).send("Email or password incorrect");
     }
+
     bcrypt.compare(password, user.password, (err, result) => {
       if (err) {
         return res.status(500).send("Internal Server Error");
@@ -68,9 +104,10 @@ app.post("/login", async (req, res) => {
       if (!result) {
         return res.status(400).send("Email or password incorrect");
       }
+
       const token = jwt.sign({ email }, "shhhh");
-      res.cookie("userToken", token);
-      res.send("you are logged in");
+      res.cookie("token", token, { httpOnly: true, path: "/" });
+      res.redirect("/courses");
     });
   } catch (err) {
     res.status(500).send("Internal Server Error");
